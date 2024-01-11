dcfb2a3679fcbc718ec135665ecaef2cff574090
delmas007 <azertyazerty1ze@gmail.com>
2024-01-11 16:54:54 +0000

    json-web-server

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products : Array<any> =[
+    {id : 1 , name : 'pc' , price : 250000, checked : false},
+    {id : 2 , name : 'climatiseur' , price : 120000, checked : true},
+    {id : 3 , name : 'phone' , price : 350000, checked : false},
+  ]
+
+  handleCheckProduct(product : any){
+    product.checked = !product.checked;
+  }
 
 }
