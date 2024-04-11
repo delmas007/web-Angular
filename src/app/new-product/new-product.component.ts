d6ef9878177d5cb720feb2fd1f45103507402ecb
delmas007 <azertyazerty1ze@gmail.com>
2024-04-11 08:48:32 +0000

    form

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './new-product.component.html',
   styleUrl: './new-product.component.css'
 })
-export class NewProductComponent {
+export class NewProductComponent implements OnInit{
+
+  public productForm!: FormGroup;
+  constructor(public fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: this.fb.control(''),
+      price: this.fb.control(''),
+      checked : this.fb.control(false),
+
+    });
+    
+  }
 
 }
