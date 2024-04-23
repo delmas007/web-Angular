a42d2c9c9ae76eac85f3c95e0cc92c852f7f3d07
delmas007 <azertyazerty1ze@gmail.com>
2024-04-23 16:51:47 +0000

    status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterLinkActive, RouterModule, RouterOutlet} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink,DashboardComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, DashboardComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  actions : Array<any> = [
-    { title : 'Home' , routes : '/home' , icon :'bi-house'},
-    { title : 'Products' , routes : '/products' , icon :'bi-search'},
-    { title : 'New products' , routes : '/newProduct' , icon :'bi-plus-circle'}
-  ]
-  currentAction : any;
 
-  setCurrentAction (action : any){
-      this.currentAction=action;
-  }
 }
