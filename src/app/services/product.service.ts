3b57e8b7f1f2fcc520e3a47fde0ef94bbfe13204
delmas007 <azertyazerty1ze@gmail.com>
2024-04-23 16:14:16 +0000

    host

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,36 +7,36 @@ import { Product } from '../model/product.model';
   providedIn: 'root',
 })
 export class ProductService {
-
+  private host: string = 'http://localhost:9090';
   constructor(private http:HttpClient) { }
 
   public getProduct():Observable<Array<Product>>{
-    return this.http.get<Array<Product>>('http://localhost:9090/api/produits')
+    return this.http.get<Array<Product>>(`${this.host}/api/produits`)
   }
 
   public checkProduct(product:Product):Observable<Product>{
-      return  this.http.patch<Product>(`http://localhost:9090/api/modifierProduit/${product.id}`,{checked:!product.checked })
+      return  this.http.patch<Product>(`${this.host}/api/modifierProduit/${product.id}`,{checked:!product.checked })
   }
 
   public searchProduct(keyword:String):Observable<Array<Product>>{
-    return this.http.get<Array<Product>>(`http://localhost:9090/api/search/${keyword}`);
+    return this.http.get<Array<Product>>(`${this.host}/api/search/${keyword}`);
   }
 
   public deleteProduct(product:number):Observable<Product>{
-    return  this.http.delete<Product>(`http://localhost:9090/api/supprimerProduit/${product}`)
+    return  this.http.delete<Product>(`${this.host}/api/supprimerProduit/${product}`)
   }
 
   saveProduct(product: Product) : Observable<Product> {
-    return this.http.post<Product>('http://localhost:9090/api/ajouterProduit', product);
+    return this.http.post<Product>(`${this.host}/api/ajouterProduit`, product);
 
   }
 
   public getProductById(productId: number):Observable<Product>{
-    return this.http.get<Product>(`http://localhost:9090/api/produit/${productId}`);
+    return this.http.get<Product>(`${this.host}/api/produit/${productId}`);
   }
 
   public updateProduct(product:Product):Observable<Product>{
-    return  this.http.put<Product>(`http://localhost:9090/api/modifierPutProduit/${product.id}`,product)
+    return  this.http.put<Product>(`${this.host}/api/modifierPutProduit/${product.id}`,product)
   }
 }
 
